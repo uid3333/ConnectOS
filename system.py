0d73545483395873eb3a49bb4e7c18b1d313974a
ЛИХОРАДКА <68346016+uid3333@users.noreply.github.com>
2025-01-05 20:30:59 +0300

    Add files via upload

---
+++ system.py
@@ -0,0 +1,48 @@
+import os, sys, time
+try:
+    from colorama import init, Fore
+    init(autoreset=True)
+except:
+    print(Fore.RED+"FATAL 003: Color module error")
+    input()
+    exit()
+def systema():
+    print('''ConnectOS: OK
+Modules: OK
+ACPI: OK
+PCI: OK
+RAM\ROM: OK''')
+    while True:
+        comma=input("""Введите команду (help для помощи)
+drv/ConnectOS>""")
+        if comma=='help':print("""help: помощь
+calc: калькулятор
+info: информация о системе
+md и rd: создать и удалить папки
+""")
+        if comma=='calc':
+            try:
+                print(eval(input('calc>')))
+            except:
+                print(Fore.YELLOW+'FAIL 006: Calculating error')
+        if comma=='md':
+            try:
+                os.mkdir(input('md>'))
+            except NotADirectoryError:
+                print(Fore.YELLOW+'FAIL 007: Invalid directory name')
+            except: print(Fore.YELLOW+'FAIL 008: Folder operation error')
+        if comma=='rd':
+            try:
+                os.rmdir(input('rd>'))
+            except NotADirectoryError:
+                print(Fore.YELLOW+'FAIL 007: Invalid directory name')
+            except: print(Fore.YELLOW+'FAIL 008: Folder operation error')
+        if comma=='death':
+            print(Fore.YELLOW+"FATAL 005: User-called error")
+            input()
+            exit()
+        if comma=='info':
+            print("""ConnectOS 1.0 (code 0)
+January 5, 2025
+by uid3333 (github)""")
+systema()
